c8530fadd50bd8db3ac6873bea6033578266cbaa
VadimKukuzia <vadimol081@gmail.com>
2020-12-29 20:16:49 +0200

    Changes in tmpl && added Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from django.utils import timezone
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    post_date = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
