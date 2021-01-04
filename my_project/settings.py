49e258e232644e53cf339372b5c0df4f0633f80d
VadimKukuzia <vadimol081@gmail.com>
2021-01-05 00:00:38 +0200

    Made password reset

--- my_project/settings.py
+++ my_project/settings.py
@@ -126,6 +126,14 @@ MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+
 try:
     from local_settings import *
 except ImportError:
